ae513467b63073733f618cbe8685d7994a36d266
Dromader2137 <wojciechowski.ignacy@proton.me>
2024-10-05 22:30:11 +0200

    atmos

--- components/planet/Planet.js
+++ components/planet/Planet.js
@@ -1,26 +1,27 @@
 import { useMemo, useRef } from "react";
+import { useFrame } from "react-three-fiber";
 import { Color } from "three";
-import { TextureLoader } from "three/src/Three.js";
+import { TextureLoader, Vector3 } from "three/src/Three.js";
 
 const planetSurfaceFragmentShader = `
 uniform vec3 u_color;
 uniform sampler2D u_surface;
-varying float distance;
-varying vec2 vUv;
+uniform vec3 u_lightDir;
+varying vec2 v_uv;
+varying vec3 v_normal;
 
 void main() {
-    float depth = distance;
-    vec3 baseColor = texture(u_surface, vUv).xyz;
-    gl_FragColor = vec4(baseColor, 1.0);
+    vec3 baseColor = texture(u_surface, v_uv).xyz;
+    gl_FragColor = vec4(baseColor * clamp(dot(v_normal, -normalize(u_lightDir)), 0.02, 1.0), 1.0);
 }
 `;
 const planetSurfaceVertexShader = `
-varying float distance;
-varying vec2 vUv;
+varying vec2 v_uv;
+varying vec3 v_normal;
 
 void main() {
-    distance = length((viewMatrix * modelMatrix * vec4(position, 1.0)).xyz);
-    vUv = uv;
+    v_uv = uv;
+    v_normal = normalize((modelMatrix * vec4(position, 1.0)).xyz);
     gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
 }
 `;
@@ -36,10 +37,14 @@ export default function Planet(props) {
       },
       u_surface: {
         value: testTexture
+      },
+      u_lightDir: {
+        value: new Vector3(-10, -6, -1)
       }
     }),
     [testTexture]
   );
+  useFrame((state, delta) => {meshRef.current.rotation.y += delta})
   return (
     <mesh
       {...props}

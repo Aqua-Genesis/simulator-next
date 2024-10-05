b5e5bbd25310f8bfda92fbe702023e886063d289
Antix7 <antekklejman@gmail.com>
2024-10-06 01:04:15 +0200

    buttons

--- app/simulation/page.js
+++ app/simulation/page.js
@@ -9,6 +9,8 @@ export default function Page() {
 
   const [planetType, setPlanetType] = useState("");
   const [values, setValues] = useState(defaultValues);
+  const [leftPanel, setLeftPanel] = useState(true);
+  const [rightPanel, setRightPanel] = useState(true);
 
 
   useEffect(() => {
@@ -20,6 +22,22 @@ export default function Page() {
     <SideButton
       text="Elements"
       position="top-right"
+      onClick={() => setRightPanel(true)}
+    />
+    <SideButton
+      text="Lifeforms"
+      position="bottom-right"
+      onClick={() => setRightPanel(false)}
+    />
+    <SideButton
+      text="Parameters"
+      position="top-left"
+      onClick={() => setLeftPanel(true)}
+    />
+    <SideButton
+      text="Overlays"
+      position="bottom-left"
+      onClick={() => setLeftPanel(false)}
     />
 
     <InputWidget
--- components/SideButton.js
+++ components/SideButton.js
@@ -1,7 +1,7 @@
-export default function SideButton({text, position, handleClick}) {
+export default function SideButton({text, position, onClick}) {
   return <button
     className={"default bg-blue2 text-gray-900 text-2xl py-1.5 px-6" + " " + position}
-    onClick={handleClick}
+    onClick={onClick}
   >
     {text}
   </button>

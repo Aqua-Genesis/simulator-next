16c861cfcc0cfe41afbec247843ac642a25a184e
Antix7 <antekklejman@gmail.com>
2024-10-05 17:50:53 +0200

    Removed <Link> from the selection menu

--- components/PlanetSelector.js
+++ components/PlanetSelector.js
@@ -1,18 +1,13 @@
 'use client'
 import Image from "next/image";
-import Link from "next/link";
 
 export default function PlanetSelector ({type, handleSelect, imgSrc, imgSize, description, colour}) {
   return <div
-    className="flex items-center w-1/3 px-8"
+    className="flex flex-col items-center justify-center w-1/3 px-8"
   >
-    <Link className="min-w-full min-h-full flex flex-col items-center justify-center"
-          href="/simulation" onClick={()=>handleSelect(type)}
-    >
-      <Image src={imgSrc} alt={imgSrc} width={imgSize} height={imgSize} />
+      <Image src={imgSrc} alt={imgSrc} width={imgSize} height={imgSize} onClick={()=>handleSelect(type)}/>
       <p className={"default text-3xl mt-4"+" text-"+colour}>{type}</p>
       <p className="default text-lg mt-2 text-center mx-8">{description}</p>
-    </Link>
   </div>
 
 }

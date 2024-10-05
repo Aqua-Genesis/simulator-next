ad28fff6572b2962c8c14bf665ffb88aa19c867a
Antix7 <antekklejman@gmail.com>
2024-10-05 18:37:24 +0200

    Description transitions

--- components/PlanetSelector.js
+++ components/PlanetSelector.js
@@ -1,13 +1,26 @@
 'use client'
 import Image from "next/image";
+import {useEffect, useRef, useState} from "react";
+import clsx from 'clsx';
 
 export default function PlanetSelector ({type, handleSelect, imgSrc, imgSize, description, colour}) {
+
+  const [descShown, setDescShown] = useState(false);
+  function handleShow() {setDescShown(true)}
+  function handleHide() {setDescShown(false)}
+
+
   return <div
-    className="flex flex-col items-center justify-center w-1/3 px-8"
+    className="flex flex-col items-center justify-center w-1/3 mx-8"
   >
-      <Image src={imgSrc} alt={imgSrc} width={imgSize} height={imgSize} onClick={()=>handleSelect(type)}/>
-      <p className={"default text-3xl mt-4"+" text-"+colour}>{type}</p>
-      <p className="default text-lg mt-2 text-center mx-8">{description}</p>
+    <Image src={imgSrc} alt={imgSrc} width={imgSize} height={imgSize} onClick={() => handleSelect(type)}
+           className="hover-trigger hover:scale-110 transition active:scale-100"
+           onMouseEnter={() => handleShow()} onMouseLeave={() => handleHide()}
+    />
+    <p className={"default text-3xl mt-8" + " text-" + colour}>{type}</p>
+    <p className={clsx("hover-receiver default text-lg mt-4 text-center mx-8 leading-tight transition",
+      descShown ? "opacity-80" : "opacity-0")}
+    >{description}</p>
   </div>
 
 }

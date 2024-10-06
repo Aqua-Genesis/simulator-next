8f1703fce343dd40a859d63cb2687e258c8a18f0
Antix7 <antekklejman@gmail.com>
2024-10-06 07:38:02 +0200

    left overlays

--- components/SideButton.js
+++ components/SideButton.js
@@ -1,6 +1,6 @@
 export default function SideButton({text, position, onClick}) {
   return <button
-    className={"default bg-window text-blue3 text-2xl z-10 py-1.5 px-6" + " " + position}
+    className={"default bg-window text-blue3 text-2xl z-20 py-1.5 px-6" + " " + position}
     onClick={onClick}
   >
     {text}
